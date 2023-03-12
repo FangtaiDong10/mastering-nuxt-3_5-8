110be82c1705e93cc39199acb04b0f9f35584155
FangtaiDong10 <fangtaidong10@gmail.com>
2023-03-12 14:07:21 +1100

    seeding database and get first Lesson endpoint update

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,43 +1,53 @@
-import {
-  Course,
-  Chapter,
-  Lesson,
-  LessonWithPath,
-} from '~/types/course';
-import course from '~/server/courseData';
+// import {
+//   Course,
+//   Chapter,
+//   Lesson,
+//   LessonWithPath,
+// } from '~/types/course';
+// import course from '~/server/courseData';
 
-course as Course;
+// course as Course;
 
-export default defineEventHandler(
-  (event): LessonWithPath => {
-    const { chapterSlug, lessonSlug } =
-      event.context.params;
+// export default defineEventHandler(
+//   (event): LessonWithPath => {
+//     const { chapterSlug, lessonSlug } =
+//       event.context.params;
 
-    const chapter: Maybe<Chapter> = course.chapters.find(
-      (chapter) => chapter.slug === chapterSlug
-    );
+//     const chapter: Maybe<Chapter> = course.chapters.find(
+//       (chapter) => chapter.slug === chapterSlug
+//     );
 
-    if (!chapter) {
-      throw createError({
-        statusCode: 404,
-        message: 'Chapter not found',
-      });
-    }
+//     if (!chapter) {
+//       throw createError({
+//         statusCode: 404,
+//         message: 'Chapter not found',
+//       });
+//     }
 
-    const lesson: Maybe<Lesson> = chapter.lessons.find(
-      (lesson) => lesson.slug === lessonSlug
-    );
+//     const lesson: Maybe<Lesson> = chapter.lessons.find(
+//       (lesson) => lesson.slug === lessonSlug
+//     );
 
-    if (!lesson) {
-      throw createError({
-        statusCode: 404,
-        message: 'Lesson not found',
-      });
-    }
+//     if (!lesson) {
+//       throw createError({
+//         statusCode: 404,
+//         message: 'Lesson not found',
+//       });
+//     }
 
-    return {
-      ...lesson,
-      path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
-    };
-  }
-);
+//     return {
+//       ...lesson,
+//       path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
+//     };
+//   }
+// );
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const { chapterSlug, lessonSlug } = event.context.params;
+
+  return prisma.lesson.findFirst();
+});
